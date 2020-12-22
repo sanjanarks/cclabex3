afedc3fb57282ac7f5793887fa21ba5c45aa86c3
sanjanarks <39239771+sanjanarks@users.noreply.github.com>
2020-12-22 10:21:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def sum();
+num1=2
+num2=3
+sum=num1+num2
+print(the sum is : , sum)
+return 0
 
 if __name__ == "__main__":
     app.run()
